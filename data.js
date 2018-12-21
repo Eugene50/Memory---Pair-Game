e63708e7cd5808342eed5d736e6005aa17cd7e51
unknown <nova_alf@ukr.net>
2018-12-21 18:36:53 +0200

    add img array

---
+++ data.js
@@ -0,0 +1,7 @@
+let images = ['<img src= img/apple.jpg>','<img src= img/banana.jpg>', 
+              '<img src= img/grusha.jpg>', '<img src= img/klubnika.jpg>', 
+              '<img src= img/malina.jpg>', '<img src= img/_apple.jpg>', 
+              '<img src= img/_banana.jpg>', '<img src= img/_grusha.jpg>',
+              '<img src= img/_klubnika.jpg>', '<img src= img/_malina.jpg>',
+              '<img src= img/_watermelon.jpg>','<img src= img/watermelon.jpg>'
+            ];
