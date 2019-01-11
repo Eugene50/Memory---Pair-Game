d071503d81ab8f8b300ca05bf53d2e94d8cbe00c
Eugene50 <nova_alf@ukr.net>
2019-01-11 15:18:57 +0200

    refactoring code

--- app.js
+++ app.js
@@ -1,31 +1,63 @@
-/* window.onload = function () {
-    const div = document.getElementById('field');
-    const img = document.getElementsByTagName('img');
-    div.addEventListener('click', function (el) {
-        console.log('click is done');
-        //document.getElementById('banana').style.visibility = 'hidden';
-    })
-} */
+const cards = document.querySelectorAll('#container');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let count = 0;
 
-window.onload = function () {
-    const container  = document.getElementById('wrapper');
+shuffle(); 
+   
+function turnOver() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-    images.sort(function() { return 0.5 - Math.random() });  //shuflle array
-    images.forEach(el => {
-        let div = document.createElement('div');
-        div.id = 'field';
-        div.innerHTML = el;
-        container.appendChild(div);
-    });
-    
-    container.addEventListener('click', function (el) {
-        
+  this.classList.add('open');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+
+  match();
+}
+   
+function match() {
+  if(firstCard.dataset.name === secondCard.dataset.name){
+    setTimeout(()=>{
+      firstCard.classList.add('close');
+      secondCard.classList.add('close');
+      count++;
+    },1400);
+  }
+  unTurnOver();
+}
+
+function unTurnOver() {
+  lockBoard = true;
+
+  setTimeout(()=> {
+    firstCard.classList.remove('open');
+    secondCard.classList.remove('open');
+
+    reset();
+  },1500);
+}
+
+function reset() {
+  if(count===6){alert('You won!');}
+
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+function shuffle() {
+    cards.forEach(element=> {
+        let randomPosition = Math.floor(Math.random() * 12);
+        element.style.order = randomPosition;
     })
 }
 
-
-
-
-
-
-/* arr.sort(function() { return 0.5 - Math.random() }); */
+cards.forEach(element => element.addEventListener('click', turnOver));
